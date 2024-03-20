0fb6ff1f0d7cba76e660ff7b7701a76057a28d6c
Andrew <drew29799@gmail.com>
2024-03-20 21:47:12 +0000

    Add SQL script that create a table with constraints
    - Created a table
    - Added 3 columns: name, id, email
    - The script does not fail if the table exists

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Creates a table `users`
+CREATE TABLE IF NOT EXISTS users(
+	id INTEGER NOT NULL UNIQUE AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY (id)
+);
