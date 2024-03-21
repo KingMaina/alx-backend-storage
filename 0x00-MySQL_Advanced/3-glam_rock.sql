05a435d22cbb312f8bc5828d99204bce3dd9926f
Andrew <drew29799@gmail.com>
2024-03-21 14:12:20 +0000

    Changed year to be dynamic

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- SQL script that retrieves the lifespan of bands with Glam Rock as their style
 SELECT band_name,
 CASE
-  WHEN split IS NULL THEN 2024 - formed
+  WHEN split IS NULL THEN YEAR(CURDATE()) - formed
   ELSE split - formed
 END AS lifespan
 FROM metal_bands
